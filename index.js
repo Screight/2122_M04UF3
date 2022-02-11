2f0076ba0158e00f3124bc6b6b76d5c0fd913486
enti <enti@enti.enti>
2022-02-11 14:23:25 +0100

    añadir movimiento coches y reseteo de coches al llegar al límite de la pantalla mas un comienzo del movimiento del jugador

--- index.js
+++ index.js
@@ -6,48 +6,20 @@ let fs = require("fs");
 
 http.createServer(function(request, response){
 
-	
-function uploadAsset(name){
+	var archivo = "index.html";
 
-	fs.readFile(name, function(error, data){
+	if(request.url != "/"){
+
+		archivo = request.url.substring(1);
+	}
+		
+	fs.readFile(archivo, function(error, data){
 
 	if(error){ console.log("Error"); return}
 
 	response.writeHead(200);
 
 	response.end(data);
-
 	});
-}
-
-if(request.url == "/character.png"){
-	uploadAsset("character.png");
-}
-
-
-if(request.url == "/character_black_red.png"){
-	uploadAsset("character_black_red.png");
-}
-
-
-if(request.url == "/character_blonde_white.png"){
-	uploadAsset("character_blonde_white.png");
-}
-
-
-if(request.url == "/character_brown_red.png"){
-	uploadAsset("character_brown_red.png");
-}
-
-
-if(request.url == "/racer_green.png"){
-	uploadAsset("racer_green.png");
-}
-
-if(request.url == "/road_background.png"){
-	uploadAsset("road_background.png");
-}
-
-uploadAsset("index.html");
 
 }).listen(1095);
